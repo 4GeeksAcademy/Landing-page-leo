369e2d41e2e0dc99a73bfa6fb3c2e140385c9bf7
KaitoKidd86 <liritokun1@gmail.com>
2025-04-23 20:25:50 +0000

    mission complete

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,45 @@
 import React from "react";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const cards = [
+  {
+    image: "https://applesanddango.wordpress.com/wp-content/uploads/2009/06/gto3.png?w=400&h=300",
+    title: "Card title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+  },
+  {
+    image: "https://lh3.googleusercontent.com/R0b3kboRX1CQJElrih1DLjNhnKvan0221a82LsS1BmTduh0Eo8QZx5K1y7G1_QNjsTU4EnoWM-5Pfnsg4QwcdUz8AAtHNuYvk4CealzqE7I8t3kO3kswNJVJPhWX5SWYkNT0cf85mvnYFaF-RA",
+    title: "Card title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+  },
+  {
+    image: "https://cosplayfu-website.s3.amazonaws.com/_Upload/b/36671-Eikichi-Cosplay-from-Great-Teacher-Onizuka.jpg",
+    title: "Card title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+  },
+  {
+    image: "https://i.pinimg.com/474x/12/f9/d1/12f9d19591d8848528a0c4e79816a914.jpg",
+    title: "Card title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+  }
+];
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Navbar />
+      <Jumbotron />
+      <div className="d-flex flex-wrap justify-content-center gap-3 p-3">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/components/jumbotron.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Jumbotron () {
+return (
+<div className="bg-light p-5 rounded text-start">
+      <h1 className="display-4">A Warm Welcome!</h1>
+      <p className="lead"></p>
+      <hr className="my-4" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac augue sit amet mauris facilisis blandit. Sed euismod ligula et arcu volutpat, id suscipit ante congue. Curabitur sollicitudin erat et sapien dapibus, nec euismod nunc lobortis. Integer a orci euismod
+      </p>
+      <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+    </div>
+
+
+
+
+
+
+
+
+);
+};
+
+export default Jumbotron;
